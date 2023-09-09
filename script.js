cf23e53c207f22968d38357110b7b0c404ff5710
Ruby Hu <60735962+HuWenShin@users.noreply.github.com>
2023-09-08 21:06:31 -0700

    Add display and endgame function
    Added display function to display player and computer choice, as well as
    score after each round. Endgame function is called when ever the player or
    the computer score reaches 5, it disables rock paper scissors option buttons
    and asks player to refresh for new game.

--- script.js
+++ script.js
@@ -11,48 +11,54 @@ function getComputerChoice() {
     }
 }
 
-let scoreCounter = 0;
+let pScore = 0;
+let cScore = 0;
 
 function playRound(playerSelection, computerSelection) {
-    // your code here!
     if (((playerSelection === "Scissors") && (computerSelection === "Rock")) || 
     ((playerSelection === "Paper") && (computerSelection === "Scissors")) ||
     ((playerSelection === "Rock") && (computerSelection === "Paper"))) {
-        console.log("Too bad you lose :(");
+        cScore += 1;
+        display_text = "Too bad you lose :(";
+        display(playerSelection, computerSelection, display_text, pScore, cScore);
     } else if (((playerSelection === "Scissors") && (computerSelection === "Paper")) ||
     ((playerSelection === "Rock") && (computerSelection === "Scissors")) ||
     ((playerSelection === "Paper") && (computerSelection === "Rock"))) {
-        scoreCounter += 1;
-        console.log("You win!");
+        pScore += 1;
+        display_text = "You win!";
+        display(playerSelection, computerSelection, display_text, pScore, cScore);
     } else {
-        console.log("It's a tie!");
+        display_text = "It's a tie, try again.";
+        display(playerSelection, computerSelection, display_text, pScore, cScore);
     }
-  }
+
+    if (pScore === 5) {
+        message = "You won the game! Refresh page to start new game.";
+        endgame("", "", message);
+    } else if (cScore === 5) {
+        message = "You lost the game. Refresh page to start new game.";
+        endgame("", "", message);
+    }
+}
+
+function display(pSel, cSel, txt, pSc, cSc) {
+    document.getElementById("playerChoice").innerText = "You chose " + pSel;
+    document.getElementById("compChoice").innerText = "Computer chose " + cSel;
+    document.getElementById("result").innerText = txt;
+    document.getElementById("playerScore").innerText = "Player Score: " + pSc;
+    document.getElementById("compScore").innerText = "Computer Score: " + cSc;
+}
    
 const button = document.querySelectorAll("button");
+
+function endgame(p, c, msg) {
+    document.getElementById("playerChoice").innerText = p;
+    document.getElementById("compChoice").innerText = c;
+    document.getElementById("result").innerText = msg;
+    button.forEach(btn => {btn.disabled = true});
+}
+
 button.forEach(btn => {btn.addEventListener("click", function() {
-    console.log(event.target.innerText);
     playRound(event.target.innerText, getComputerChoice());
     })
 });
-
-
-
-// function game() {
-//     for (i = 0; i < 5; i ++) {
-//         let playerChoice = c;
-        
-//         let computerChoice = getComputerChoice();
-//         console.log(playerChoice);
-//         console.log(computerChoice);
-//         console.log(playRound(playerChoice, computerChoice));
-//     }
-//     console.log("Your score: " + scoreCounter + "/5");
-//     if (scoreCounter >= 3) {
-//         console.log("You won the game!")
-//     } else {
-//         console.log("Try harder next time.")
-//     }
-// }
-
-// game();
